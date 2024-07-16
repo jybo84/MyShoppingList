aa85e72e39437ea5ecb93b24af98ed4ebb73dd9a
Maxim <jybo@mail.ru>
2024-07-16 14:37:25 +1000

    Num-5 работа с кодом

--- app/src/main/java/com/example/myshoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/myshoppinglist/presentation/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.example.myshoppinglist.presentation
 
 import android.os.Bundle
-import android.util.Log
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 import com.example.myshoppinglist.R
+import com.example.myshoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainActivityViewModel
 //     var viewModel: MainActivityViewModel by viewModels() TODO() почему так не работает
 
-    val ll_shop_item by lazy {  }
+    private lateinit var llShopItem: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,10 +28,31 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
+        llShopItem = findViewById(R.id.ll_shop_item)
         viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
         viewModel.shopList.observe(this) {
-            Log.i("!!!", it.toString())
+            showList(it)
+
+        }
+    }
+
+    private fun showList(list: List<ShopItem>) {
+        llShopItem.removeAllViews()
+        for (el in list) {
+            val card = if (el.enabled)
+                R.layout.item_shop_enabled
+            else
+                R.layout.item_shop_disabled
+
+            val createView = LayoutInflater.from(this).inflate(card, llShopItem, false)
+            val count = createView.findViewById<TextView>(R.id.tv_count)
+            count.text = el.count.toString()
+
+            createView.setOnLongClickListener {
+                viewModel.changeEnableState(el)
+                true
+            }
+                llShopItem.addView(createView)
 
         }
     }
