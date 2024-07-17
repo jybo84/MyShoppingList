829a2f6dfecc780786f878d293bc7766fc10d432
Maxim <jybo@mail.ru>
2024-07-17 16:04:30 +1000

    Num-7 увеличен размер ПУЛа RV

--- app/src/main/java/com/example/myshoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/example/myshoppinglist/presentation/ShopListAdapter.kt
@@ -51,5 +51,6 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopListViewHolder>
     companion object {
         const val VIEW_TYPE_ENABLED = 1
         const val VIEW_TYPE_DISABLED = 0
+        const val MAX_POOL_SIZE = 10
     }
 }
