90bc71380542ce033ed9030019e3b0fc09826e20
Maxim <jybo@mail.ru>
2024-07-18 22:09:04 +1000

    Num-8 создан класс callback

---
+++ app/src/main/java/com/example/myshoppinglist/presentation/ShopClassDiffCallback.kt
@@ -0,0 +1,30 @@
+package com.example.myshoppinglist.presentation
+
+import androidx.recyclerview.widget.DiffUtil
+import com.example.myshoppinglist.domain.ShopItem
+
+class ShopClassDiffCallback(
+    private val oldList: List<ShopItem>,
+    private val newList: List<ShopItem>,
+) : DiffUtil.Callback() {
+
+    override fun getOldListSize(): Int {
+        return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newList.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldList = oldList[oldItemPosition]
+        val newList = newList[newItemPosition]
+        return oldList.id == newList.id
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldList = oldList[oldItemPosition]
+        val newList = newList[newItemPosition]
+        return oldList == newList
+    }
+}
